d5533570d80cbe04fc4ade8c95ed9aae6eaaf17e
AVATAR ID <68086971+romli2020@users.noreply.github.com>
2020-10-10 03:57:43 +0700

    Add files via upload

---
+++ anonymous_style.py
@@ -0,0 +1,119 @@
+#coding=utf-8
+import os,sys,time
+def teks(wkwk):
+    for haha in wkwk + '\n':
+        sys.stdout.write(haha)
+        sys.stdout.flush()
+        time.sleep(0.0001)
+
+os.system("clear")
+teks("""
+                        `/ymMMMMMMMMMmy/`
+                     `/ymMMMMMMMMMMMMMMMmy/`
+                   /hMMMMMMMMMMMMMMMMMMMMMMMh/
+                 /mMMmNMMMMMNNNNNNNNNMMMMMNNMMm/
+               `hNmo:dMMNNNmNNmNmNmNNmNNNMMd:omNh`
+              .mh:+/hMNNNNmNNmNdohmmNNmNNNNMy/o:hm.
+             `d+://sNmMMMmMMMhM++MmMMMmMMnMmNs///+d`
+             ys.o/oMmNNNmNNMNNMmmdMNNMNNmNNNmMo/o.ys
+            `my.-/NmMMMMmMMNmNNyiyNNmNMMmMMMMmN/:`ym`
+            -h/+s/MmMMMNmNNNdyY+++HmNNNmNMnMNmM:so/h-
+            -N.o.sMmMMMNh/:-`-MoHsM-`-:/hNMMMmMs.+.N-
+            `ho/-ohmMMMM/    -M/v+M.    /MMMMmho-/oh`
+             s+-s-odmNNN`     /-v:/     .NNNmd+:s-+s
+             `mo/-:+ymMm                 mMms+:-/om`
+              .h/+/y`hhs                 yyh`y/+/h.
+               `hd/::-+.                 .+-::/dy`
+                 /hs+/::.--           --.::/+sh:
+                   :hds+/-`           `-/+sdh:
+                     `/xmM+           +Mmx/`
+                        [-  YOUR NAME  -]
+
+                .__=[ Username ° YOUR NAME  ]=__.
+     °×-- •• ••=[ I DO THIS BECAUSE I CAN DO IT ]=•• •• --×°
+""")
+
+     #
+    ##
+   ###
+   ###
+   ###
+   ###             #
+   ###            ##
+   ###           ###
+#########        ###
+ #######         ###
+  #####          ###
+   ###           ###
+    #         #########
+               #######
+                #####
+                 ###
+                  #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################################
+##       Style By Din-zUgex95       ##
+## Subscibe youtube.com/Din-zUgex95 ##
+######################################
+
+######################################
+##       Style By Din-zUgex95       ##
+## Subscibe youtube.com/Din-zUgex95 ##
+######################################
